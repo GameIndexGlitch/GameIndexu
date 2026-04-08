c61acdcffcfc40ee48ef68e0d7c967ba80c12074
Petersjonny <pedrojulio.barreto@gmail.com>
2026-04-07 21:52:44 -0300

    Sprite Irís parada

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,10 +1,22 @@
 export class Player {
-    constructor(color, physics) {
+    constructor(color, physics, characterName) {
         this.x = 100; //Posição horizontal inicial no Vazio [cite: 3]
         this.y = 500; //Posição vertical inicial acima da plataforma
         this.width = 50; //Largura do retângulo (espaço para sua sprite futura)
         this.height = 80; //Altura do retângulo (espaço para sua sprite futura)
         this.color = color; //Cor definida pela escolha na SelectionScene
+        this.characterName = characterName; // Armazena nome
+
+        // Configuração da Sprite da Iris
+        this.irisSprite = new Image();
+        this.irisSprite.src = 'assets/sprites/cartas_select/SpriteSheetIdleIris.png'
+        this.irisLoad = false;
+        this.irisSprite.onload = () => { this.irisLoad = true; };
+
+        this.numberOfFrames = 10; // Quantidade de frames
+        this.frameIndex = 0; // Frame atual
+        this.tickCount = 0; // Contador de frames de atualização
+        this.ticksPerFrame = 6; // Velocidade que passa os frames
 
         this.velocityX = 0; //Velocidade lateral atual
         this.velocityY = 0; //Velocidade vertical (gravidade/pulo)
@@ -16,6 +28,16 @@ export class Player {
     }
 
     update() {
+        // atualiza a animação
+        if (this.characterName == 'Irís Shadowlace') {
+            this.tickCount++;
+            if(this.tickCount > this.ticksPerFrame) {
+                this.tickCount = 0;
+                // Avança frame e volta pro inicio quando chega a zero
+                this.frameIndex = (this.frameIndex + 1) % this.numberOfFrames;
+            }
+        }
+
         // Aplica física usando o sistema Physics.js
         if (this.physics) {
             this.physics.applyPhysics(this);
@@ -35,12 +57,27 @@ export class Player {
     }
 
     draw(ctx) {
-        // Desenha o retângulo colorido representando o doidão escolhido [cite: 80-85]
-        ctx.fillStyle = this.color; //Define a cor (Fogo, Metal, Madeira, etc)
-        ctx.fillRect(this.x, this.y, this.width, this.height); //Pinta o personagem na tela
+        // Verifica se é a Irís E se ela está parada (velocidades zeradas e no chão)
+        const estaParada = this.velocityX === 0 && this.velocityY === 0 && this.isGrounded;
 
-        // Espaço reservado para o desenho feito à mão futuramente
-        // ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+        if (this.characterName === 'Irís Shadowlace' && this.irisLoad && estaParada) {
+            // Desenha apenas parada
+            const frameWidth = this.irisSprite.width / this.numberOfFrames;
+            const frameHeight = this.irisSprite.height;
+
+            ctx.drawImage(
+                this.irisSprite,
+                this.frameIndex * frameWidth, 0,
+                frameWidth, frameHeight,
+                this.x, this.y,
+                this.width, this.height
+            );
+
+        } else {
+            // Desenha o bloco colorido
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 
     jump() {
